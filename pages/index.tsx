ea656bf5cede0cf0f790061274c1dce4d967b23d
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2022-12-23 10:58:09 +0100

    darktheme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import { NextPage } from 'next';
+import { Button } from '@nextui-org/react'
 
 const HomePage: NextPage = () => {
   return (
     <>
+    <Button color='gradient'>
       <h1>Hello World</h1>
+    </Button>
     </>
   );
 };
