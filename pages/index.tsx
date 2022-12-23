0f3d1ed62b409bb188fa402c1f30485d7847c356
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2022-12-23 16:20:24 +0100

    getStaticProps + fetchApi + res Types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { Button } from "@nextui-org/react";
 import { MainLayout } from "../components/layouts";
+import { pokeApi } from "../api";
+import { PokemonListResponse } from "../interfaces";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+
+  console.log(props)
   return (
     <>
       <MainLayout title="Listado de Pokemones">
@@ -13,4 +17,17 @@ const HomePage: NextPage = () => {
     </>
   );
 };
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  
+  const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
+  console.log(data)
+  return{
+    props:{
+      pokemons: data.results
+    }
+  }
+}
+
+
 export default HomePage;
